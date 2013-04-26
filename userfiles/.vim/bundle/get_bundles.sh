178fb6d7ef64067a7a18c1deb4322d68f01c526e
Sebastian <sebastian@devunit.eu>
2013-04-26 14:12:19 +0200

    fix get_bundles.sh

--- userfiles/.vim/bundle/get_bundles.sh
+++ userfiles/.vim/bundle/get_bundles.sh
@@ -7,12 +7,13 @@ clone_repo() {
 	dest_dir=$2
 
 	if [[ -e $dest_dir ]]; then
+		echo "Destination exists $dest_dir"
 		return 0
 	fi
 
 	git clone "$repo_url" "$dest_dir"
 }
 
-clone_repo https://github.com/scrooloose/nerdtree.git $BASEDIR
-clone_repo git://github.com/altercation/vim-colors-solarized.git $BASEDIR
+clone_repo https://github.com/scrooloose/nerdtree.git $BASEDIR/nerdtree
+clone_repo git://github.com/altercation/vim-colors-solarized.git $BASEDIR/vim-colors-solarized
 
--- userfiles/init_user.sh
+++ userfiles/init_user.sh
@@ -5,7 +5,7 @@ BASEDIR=$(dirname $0)
 for file in $BASEDIR/.*; do
 	FILENAME=`basename $file`
 	if [ $FILENAME != "." ] && [ $FILENAME != ".." ]; then
-		if [ -a $file ]; then 
+		if [ -a ~/$FILENAME  ]; then 
 			echo "File ~/$FILENAME already exist"
 		else
 			echo "Link $file to $FILENAME"
