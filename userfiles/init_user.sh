ce911e5bb683316eff813f8a8a85a50eca513cd2
Sebastian <sebastian@devunit.eu>
2013-04-26 12:25:28 +0200

    added installation scripts for new user and vim bundles

---
+++ userfiles/init_user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+
+for file in $BASEDIR/.*; do
+	FILENAME=`basename $file`
+	if [ $FILENAME != "." ] && [ $FILENAME != ".." ]; then
+		if [ -a $file ]; then 
+			echo "File ~/$FILENAME already exist"
+		else
+			echo "Link $file to $FILENAME"
+			ln -s $file ~/$FILENAME
+		fi
+	fi
+done
