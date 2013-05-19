0244a879da9d3f84cee9327a5af8c1d9ddfe7103
Sebastian <sebastian@devunit.eu>
2013-05-20 01:02:49 +0200

    fix error in init_user.sh

--- userfiles/init_user.sh
+++ userfiles/init_user.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-SIMULATE=$1
-
 create_linksInDir() {
 	DEST=$1
-
 	for file in $2; do			
 		FILENAME=`basename $file`
 		EXT="${FILENAME##*.}"
 		if [ $FILENAME != "." ] && [ $FILENAME != ".." ] && 
 		   [ $FILENAME != ".config" ] && [ $EXT != "swp" ]; then
-			if [ -a $DEST/$FILENAME  ]; then 
-				echo "File $DEST/$FILENAME already exist"
-			else
-				echo "Link $file to $DEST/$FILENAME"
-				if [ $SIMULATE  -eq 0 ]; then 
-					ln -s $file $DEST/$FILENAME
-				fi
+			if [ ! -e $DEST/$FILENAME  ]; then 
+				echo "ln -s $file ${DEST}/$FILENAME"
 			fi
 		fi
 	done
@@ -31,5 +23,5 @@ fi
 
 BASEDIR=${SCRIPTPATH}/${BASEDIR}
 
-create_linksInDir "~" "${BASEDIR}/.*"
-create_linksInDir "~/.config" "${BASEDIR}/.config/*"
+create_linksInDir ~ "${BASEDIR}/.*"
+create_linksInDir ~/.config "${BASEDIR}/.config/*"
