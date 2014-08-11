0fe522badc4e420575d9184d0c5f826bc4fbe710
sebastian <sebastian@devunit.eu>
2014-08-11 20:09:33 +0200

    skippded links in output

--- init.py
+++ init.py
@@ -21,50 +21,75 @@ def create_arguments():
 
 
 def get_links():
+    current_links = {}
+    current_skipped_links = {}
+
     links = {}
+    skipped_links = {}
 
     if args.graphical:
         graphical_dir = os.path.join(SCRIPT_DIR, 'graphical')
-        links = dict(list(links.items()) + list(get_links_from_dir(graphical_dir).items()))
+
+        current_links, current_skipped_links = get_links_from_dir(graphical_dir)
+
+        links = dict(list(links.items()) + list(current_links.items()))
+        skipped_links = dict(list(skipped_links.items()) + list(current_skipped_links.items()))
 
     if args.terminal:
         terminal_dir = os.path.join(SCRIPT_DIR, 'terminal')
-        links = dict(list(links.items()) + list(get_links_from_dir(terminal_dir).items()))
+
+        current_links, current_skipped_links = get_links_from_dir(terminal_dir)
+
+        links = dict(list(links.items()) + list(current_links.items()))
+        skipped_links = dict(list(skipped_links.items()) + list(current_skipped_links.items()))
 
     if args.editor:
         editor_dir = os.path.join(SCRIPT_DIR, 'editor')
-        links = dict(list(links.items()) + list(get_links_from_dir(editor_dir).items()))
 
-    return links
+        current_links, current_skipped_links = get_links_from_dir(editor_dir)
+
+        links = dict(list(links.items()) + list(current_links.items()))
+        skipped_links = dict(list(skipped_links.items()) + list(current_skipped_links.items()))
+
+    return links, skipped_links
 
 
 def get_links_from_dir(dir_path):
     links = {}
+    skipped_links = {}
     for item in os.listdir(dir_path):
         source_file = os.path.join(dir_path, os.path.basename(item))
         dest_file = os.path.join(USER_HOME_DIR, os.path.basename(item))
 
         if not args.check or (args.check and not os.path.islink(dest_file)):
             links[source_file] = dest_file
+        else:
+            skipped_links[source_file] = dest_file
 
-    return links
+    return links, skipped_links
 
 
 def print_link():
-    links = get_links()
+    links, skipped_links = get_links()
 
     for key, value in links.items():
         print('ln -s ' + key + ' ' + value)
 
+    for key, value in skipped_links.items():
+        print('link for ' + key + ' skipped')
+
 
 def link_files():
-    links = get_links()
+    links, skipped_links = get_links()
 
     for key, value in links.items():
         if args.verbose:
             print('linking ' + key + ' to ' + value)
         os.symlink(key, value)
 
+    if args.verbose:
+        for key, value in skipped_links.items():
+            print('link for ' + key + ' skipped')
 
 if __name__ == "__main__":
     SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
