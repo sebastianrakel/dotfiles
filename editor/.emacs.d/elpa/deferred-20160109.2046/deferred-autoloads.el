66c43500c0bde4ac1d67d2543acbdd5ad747da6e
Sebastian Rakel <sebastian@devunit.eu>
2016-10-14 14:54:35 +0200

    Fix repo

--- editor/.emacs.d/elpa/deferred-20160109.2046/deferred-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; deferred-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/deferred-20160109.2046/deferred-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/deferred-20160109.2046/deferred.el")
-;;;;;;  (22523 40641 539449 159000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; deferred-autoloads.el ends here
--- editor/.emacs.d/elpa/python-environment-20150310.153/python-environment-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; python-environment-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/python-environment-20150310.153/python-environment-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/python-environment-20150310.153/python-environment-pkg.el"
-;;;;;;  "../../../../../.emacs.d/elpa/python-environment-20150310.153/python-environment.el"
-;;;;;;  "../../../../../.emacs.d/elpa/python-environment-20150310.153/test-python-environment.el")
-;;;;;;  (22523 40641 989449 150000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; python-environment-autoloads.el ends here
