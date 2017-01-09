13be6d808050c1bc35c393f5c054f0214bee1fef
Sebastian Rakel <sebastian@devunit.eu>
2017-01-09 10:28:40 +0100

    Clean Up

--- editor/.emacs.d/init.el
+++ editor/.emacs.d/init.el
@@ -20,7 +20,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-dash arduino-mode sublimity helm-gitlab chess confluence go-playground go-mode org-bullets auto-complete use-package base16-theme))))
+    (omnisharp helm-dash arduino-mode sublimity helm-gitlab chess confluence go-playground go-mode org-bullets auto-complete use-package base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
