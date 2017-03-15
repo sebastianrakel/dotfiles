184c77ff8f28bc6c9589832d3ee8189e778b193c
Sebastian Rakel <sebastian@devunit.eu>
2017-03-15 18:51:12 +0100

    New emacs stuff

--- editor/.emacs.d/init.el
+++ editor/.emacs.d/init.el
@@ -20,7 +20,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (php-mode ng2-mode omnisharp helm-dash arduino-mode sublimity helm-gitlab chess confluence go-playground go-mode org-bullets auto-complete use-package base16-theme))))
+    (nginx-mode ansible pony-mode python-django jinja2-mode php-mode ng2-mode omnisharp helm-dash arduino-mode sublimity helm-gitlab chess confluence go-playground go-mode org-bullets auto-complete use-package base16-theme)))
+ '(safe-local-variable-values
+   (quote
+    ((pony-settings
+      (make-pony-project :python "/home/sebastian/Projects/hodor/venv/bin/python" :pythonpath "/home/sebastian/Projects/hodor/venv/lib" :settings "settings" :appsdir "/home/sebastian/Projects/hodor"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
