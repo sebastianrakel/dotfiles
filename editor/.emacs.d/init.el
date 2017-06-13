8944c4ba859c7e4fdb662169630578cb418f3944
Sebastian Rakel <sebastian@devunit.eu>
2017-06-13 13:44:33 +0200

    Update emacs config

--- editor/.emacs.d/init.el
+++ editor/.emacs.d/init.el
@@ -2,8 +2,10 @@
 
 (setq package-enable-at-startup nil)
 
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
 
@@ -18,9 +20,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" default)))
  '(package-selected-packages
    (quote
-    (nginx-mode ansible pony-mode python-django jinja2-mode php-mode ng2-mode omnisharp helm-dash arduino-mode sublimity helm-gitlab chess confluence go-playground go-mode org-bullets auto-complete use-package base16-theme)))
+    (haml-mode nginx-mode ansible pony-mode python-django jinja2-mode php-mode ng2-mode omnisharp helm-dash arduino-mode sublimity helm-gitlab chess confluence go-playground go-mode org-bullets auto-complete use-package base16-theme)))
  '(safe-local-variable-values
    (quote
     ((pony-settings
