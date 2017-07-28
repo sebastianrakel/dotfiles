9591980717122f437c02558e6ce3d72a01e49b58
Sebastian Rakel <sebastian@devunit.eu>
2017-07-28 18:37:16 +0200

    Add rofi scripts

---
+++ bin/rofi-extension/lib/rofi.rb
@@ -0,0 +1,34 @@
+DMENU_OPTS = %w(-dmenu -columns 1)
+SEPERATOR = '-' * 40
+
+def dmenu(items, prompt=nil)
+    args = Array.new
+    hash = Hash.new
+
+    if prompt
+        args.push('-p')
+        args.push("'#{prompt} -> '")
+    end
+
+    args += DMENU_OPTS
+
+    input = ""
+
+    index = 1
+
+    items.each do |key, value|
+        line = "#{index}. #{value.to_s}\n"
+        input += line 
+
+        hash[line] = key
+
+        index = index + 1
+    end
+
+    command = 'rofi ' + args.join('  ')
+
+    o, e, s = Open3.capture3(command, :stdin_data => input)
+   
+    return hash[o]
+end
+
