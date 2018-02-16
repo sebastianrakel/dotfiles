2670d926c691db4b425685559839123c3318d651
Sebastian Rakel <sebastian@devunit.eu>
2018-02-16 11:04:57 +0100

    Update prompt stuff

--- terminal/.zsh/prompt.zsh
+++ terminal/.zsh/prompt.zsh
@@ -6,10 +6,12 @@ autoload -Uz vcs_info
 promptinit
 
 zstyle ':vcs_info:*' enable hg git bzr svn
+
+is_my_project=""
 precmd() {
-  vcs_info
+    vcs_info
 }
-zstyle ':vcs_info:git*' formats "(%s) %b %m%u%c "
+zstyle ':vcs_info:git*' formats "%{$fg[red]%}${is_my_project}%{$reset_color%}  %{$fg[red]%}%b%{$reset_color%} %m%u%c "
 zstyle ':vcs_info:*' check-for-changes true
 
 setopt prompt_subst
