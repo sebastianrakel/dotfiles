4e7b4ea829db02a8f0512004b31064db8a13dd83
Sebastian Rakel <sebastian@devunit.eu>
2018-08-06 01:24:58 +0200

    Update for first new version of arch versioning

--- bin/archlinux_check_kernel.sh
+++ bin/archlinux_check_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-OUTPUT=${PRINT:-"1"} perl -E '$output = $ENV{"OUTPUT"} == "1"; ($run, $inst) = (`uname -a` =~ /([\w\d-.]+)-ARCH/g, `pacman -Qi linux` =~ /Version\s+:\s+(.*)/g); say "Running Kernel: $run" if $output ; say "Installed Kernel: $inst" if $output ; exit 1 if $run ne $inst'
+OUTPUT=${PRINT:-"1"} perl -E '$output = $ENV{"OUTPUT"} == "1"; ($run, $run_patch, $inst) = (`uname -r` =~ /([\w\d-.]+)-arch([\d]+)/g, `pacman -Q linux` =~ /linux\s+(.*)/g); say "Running Kernel: $run-$run_patch" if $output ; say "Installed Kernel: $inst" if $output ; exit 1 if "#{run}-#{run_patch}" != "#{inst}"'
 
 rc=$?
-
+ 
 if [[ $rc != 0 ]]; then
     echo "YOUR FUCKING KERNEL IS FUCKING OLD!"
 fi
