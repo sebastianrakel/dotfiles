3407d9d9c8cb9bb699e6683ccb02503815598d80
Sebastian Rakel <sebastian@devunit.eu>
2018-09-10 08:25:05 +0200

    Update emacs settings

--- editor/.emacs.d/init.el
+++ editor/.emacs.d/init.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" default)))
+    ("aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" default)))
  '(package-selected-packages
    (quote
     (crystal-mode notmuch rust-mode org-jira android-mode gradle-mode scad-mode scad-preview treemacs puppet-mode flymake-puppet yaml-mode which-key use-package smartparens restclient rainbow-delimiters powerline org-bullets nyan-mode markdown-mode magit lua-mode json-mode jedi helm-projectile helm-dash haskell-mode haml-mode groovy-mode go-playground go-dlv go-autocomplete dockerfile-mode base16-theme))))
