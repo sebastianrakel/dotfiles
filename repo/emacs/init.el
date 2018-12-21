652d4245b14dbd146d6460d8b4afa063a55cc102
Sebastian Rakel <sebastian@devunit.eu>
2018-12-21 21:26:35 +0100

    Update some stuff

--- repo/emacs/init.el
+++ repo/emacs/init.el
@@ -25,10 +25,11 @@
     ("aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" default)))
  '(package-selected-packages
    (quote
-    (crystal-mode notmuch rust-mode org-jira android-mode gradle-mode scad-mode scad-preview treemacs puppet-mode flymake-puppet yaml-mode which-key use-package smartparens restclient rainbow-delimiters powerline org-bullets nyan-mode markdown-mode magit lua-mode json-mode jedi helm-projectile helm-dash haskell-mode haml-mode groovy-mode go-playground go-dlv go-autocomplete dockerfile-mode base16-theme))))
+    (crystal-playground dired-single ox-reveal epresent org-tree-slide chess twittering-mode nginx-mode anaconda-mode pony-mode crystal-mode notmuch rust-mode org-jira android-mode gradle-mode scad-mode scad-preview treemacs puppet-mode flymake-puppet yaml-mode which-key use-package smartparens restclient rainbow-delimiters powerline org-bullets nyan-mode markdown-mode magit lua-mode json-mode jedi helm-projectile helm-dash haskell-mode haml-mode groovy-mode go-playground go-dlv go-autocomplete dockerfile-mode base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'dired-find-alternate-file 'disabled nil)
