832a963689e11f22c67507236299f0128d0f1291
Sebastian Rakel <sebastian@devunit.eu>
2019-01-09 11:36:27 +0100

    Use smaller script from Bluewind to test if kernel is old

--- repo/scripts/files/archlinux_check_kernel.sh
+++ repo/scripts/files/archlinux_check_kernel.sh
@@ -1,14 +1,2 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-use List::Util qw/any/;
-use feature qw/say/;
-
-my @old = `uname -r` =~ /(\d+)/g;
-my @new = `pacman -Q linux` =~ /(\d+)/g;
-
-my $reboot = @old != @new || any {$old[$_] != $new[$_]} (0..(@new-1));
-
-if ($reboot) {
-       print "YOUR FUCKING KERNEL IS FUCKING OLD!\n";
-}
+#!/bin/bash
+test -d /lib/modules/$(uname -r)/kernel/ || echo 'YOUR FUCKING KERNEL IS FUCKING OLD!'
