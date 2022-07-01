5a9b5da15d1a986ab6df51c64da5e27da1e31b19
Sebastian Rakel <sebastian.rakel@service-erp.de>
2022-07-01 14:45:27 +0200

    Use Comic Mono!

--- repo/font/install_comic_mono.sh
+++ repo/font/install_comic_mono.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 echo "Check Comic Font"
 base=https://raw.githubusercontent.com/dtinth/comic-mono-font/master/
+comic_shanns_source=https://github.com/shannpersand/comic-shanns/raw/master/v2/comic%20shanns%202.ttf
 comic_bold="${HOME}/.local/share/fonts/ComicMono-Bold.ttf"
 comic="${HOME}/.local/share/fonts/ComicMono.ttf"
+comic_shanns="${HOME}/.local/share/fonts/comic shanns 2.ttf"
 
 mkdir -p "${HOME}/.local/share/fonts"
 
@@ -12,4 +14,8 @@ fi
 
 if [ ! -e "${comic_bold}" ]; then
     curl -L "${base}ComicMono-Bold.ttf" -o "${comic_bold}"
+fi
+
+if [ ! -e "${comic_shanns}" ]; then
+    curl -L "${comic_shanns_source}" -o "${comic_shanns}"
 fi
