93690b089f2892e1d1ac7a08290922d19c337a07
Sebastian Rakel <sebastian@devunit.eu>
2022-09-26 14:07:09 +0200

    Fix bug awesome

--- repo/font/install_awesome.sh
+++ repo/font/install_awesome.sh
@@ -2,6 +2,11 @@
 
 echo "Check for Awesome 5"
 
+if [[ -d "${HOME}/.local/share/fonts/fontawesome-free-5.15.4-desktop/" ]]; then
+    echo "FontAwesome already installed"
+    exit 0
+fi
+
 mkdir -p "${HOME}/.local/share/fonts"
 
 curl -L "https://use.fontawesome.com/releases/v5.15.4/fontawesome-free-5.15.4-desktop.zip" -o /tmp/awesome.zip
