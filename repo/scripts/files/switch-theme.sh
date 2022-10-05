6d7e762632e8b7c11f860c307eb20142b5c90438
Sebastian Rakel <sebastian@devunit.eu>
2022-10-05 21:41:15 +0200

    More styling, going new ways

--- repo/scripts/files/switch-theme.sh
+++ repo/scripts/files/switch-theme.sh
@@ -2,12 +2,22 @@
 
 theme="${1}"
 
+if [ -z "${LIGHT_THEME}" ]; then
+    echo "LIGHT_THEME not set"
+    exit 1
+fi
+
+if [ -z "${DARK_THEME}" ]; then
+    echo "DARK_THEME not set"
+    exit 1
+fi
+
 case "${theme}" in
     "light")
-	changing_theme="gruvbox-light-medium"
+	changing_theme="${LIGHT_THEME}"
 	;;
     "dark")
-	changing_theme="dracula"
+	changing_theme="${DARK_THEME}"
 	;;
     *)
 	echo "Wrong theme, choose light/dark"
@@ -21,8 +31,8 @@ emacsclient -e "(switch-theme '${changing_theme})"
 echo "Switching alacritty theme"
 alacritty-theme-switch "${changing_theme}"
 echo "Switching polybar theme"
-polybar-theme-switch "${changing_theme}"
+polybar-theme-switch "dracula"
 echo "Switching hlwm theme"
-hlwm-theme-switch.sh "${changing_theme}"
+hlwm-theme-switch.sh "dracula"
 
 exit 0
