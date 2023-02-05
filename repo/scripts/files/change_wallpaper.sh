2cb0e3b5e7d81768d73ba71646e39f6f529f1d07
Sebastian Rakel <sebastian@devunit.eu>
2023-02-05 23:35:40 +0100

    New Wallpaper stuff

--- repo/scripts/files/change_wallpaper.sh
+++ repo/scripts/files/change_wallpaper.sh
@@ -3,7 +3,7 @@
 if [ ! -z "${1}" ]; then
     wallpaper="${1}"
 else
-    wallpaper=$(find "${HOME}/Pictures/Wallpaper/" -type f -print0 | shuf -z -n 1)
+    wallpaper=$(find "${HOME}/Pictures/Wallpaper/" -maxdepth 1 -type f -print0 | shuf -z -n 1)
 fi
 
 echo "Switch to wallpaper: ${wallpaper}"
