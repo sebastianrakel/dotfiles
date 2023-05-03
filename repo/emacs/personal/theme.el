7d9f63ad2019777d1e44adf57861f0efe0a7da53
Sebastian Rakel <sebastian@devunit.eu>
2023-05-03 22:19:32 +0200

    Update themes

--- repo/emacs/personal/theme.el
+++ repo/emacs/personal/theme.el
@@ -2,3 +2,4 @@
 (defvar current-theme 'base16-penumbra-balanced-eight-light)
 (defvar current-theme 'base16-gruvbox-dark-hard)
 (defvar current-theme 'base16-penumbra-balanced-eight-light)
+(defvar current-theme 'base16-penumbra-balanced-eight-dark)
