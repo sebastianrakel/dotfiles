5283b9d237913b7ae61afcd73cde8948886b536c
Sebastian Rakel <sebastian@devunit.eu>
2023-05-06 09:46:17 +0200

    Fix power script

--- repo/rofi/config/modes/power.sh
+++ repo/rofi/config/modes/power.sh
@@ -3,21 +3,25 @@
 if [ x"${@}" = x"reboot" ]
 then
     systemctl reboot
+    exit 0
 fi
 
 if [ x"${@}" = x"suspend" ]
 then
     systemctl suspend
+    exit 0
 fi
 
 if [ x"${@}" = x"shutdown" ]
 then
     systemctl poweroff
+    exit 0
 fi
 
 if [ x"${@}" = x"hlwm quit" ]
 then
     herbstclient quit
+    exit 0
 fi
 echo "reboot"
 echo "suspend"
