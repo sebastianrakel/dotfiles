5fb082fbbf986069df86e9975db6e6c86842f2c0
Sebastian Rakel <sebastian@devunit.eu>
2023-07-27 22:21:06 +0200

    Add fast workspace switch

---
+++ repo/rofi/config/modes/workspaces.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+workspace="${1}"
+hostname=$(hostname)
+config_dir="${HOME}/.config/workspaces/"
+
+if [[ -z "${workspace}" ]]; then
+    workspaces="${config_dir}"$hostname_*.sh
+    for file in $workspaces
+    do
+	workspace_name=$(basename "${file}")
+	workspace_name="${workspace_name/.sh/}"
+	workspace_name="${workspace_name/"${hostname}_"/}"
+	echo $workspace_name
+    done
+    exit 0
+fi
+
+source "${config_dir}${hostname}_${workspace}.sh"
+exit 0
