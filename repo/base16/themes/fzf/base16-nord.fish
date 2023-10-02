f63c43632833695227d7b76caf4697283b986e46
Sebastian Rakel <sebastian@devunit.eu>
2023-10-02 20:37:09 +0200

    More themes!!!

---
+++ repo/base16/themes/fzf/base16-nord.fish
@@ -0,0 +1,33 @@
+# Base16 Nord
+# Scheme author: arcticicestudio
+# Template author: Tinted Theming (https://github.com/tinted-theming)
+
+set -l color00 '#2E3440'
+set -l color01 '#3B4252'
+set -l color02 '#434C5E'
+set -l color03 '#4C566A'
+set -l color04 '#D8DEE9'
+set -l color05 '#E5E9F0'
+set -l color06 '#ECEFF4'
+set -l color07 '#8FBCBB'
+set -l color08 '#BF616A'
+set -l color09 '#D08770'
+set -l color0A '#EBCB8B'
+set -l color0B '#A3BE8C'
+set -l color0C '#88C0D0'
+set -l color0D '#81A1C1'
+set -l color0E '#B48EAD'
+set -l color0F '#5E81AC'
+
+set -l FZF_NON_COLOR_OPTS
+
+for arg in (echo $FZF_DEFAULT_OPTS | tr " " "\n")
+    if not string match -q -- "--color*" $arg
+        set -a FZF_NON_COLOR_OPTS $arg
+    end
+end
+
+set -Ux FZF_DEFAULT_OPTS "$FZF_NON_COLOR_OPTS"\
+" --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
+" --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
+" --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
---
+++ repo/base16/themes/herbstluftwm/base16-nord.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# Nord
+
+hlwm_base00='#2E3440'
+hlwm_base01='#3B4252'
+hlwm_base02='#434C5E'
+hlwm_base03='#4C566A'
+hlwm_base04='#D8DEE9'
+hlwm_base05='#E5E9F0'
+hlwm_base06='#ECEFF4'
+hlwm_base07='#8FBCBB'
+hlwm_base08='#BF616A'
+hlwm_base09='#D08770'
+hlwm_base0A='#EBCB8B'
+hlwm_base0B='#A3BE8C'
+hlwm_base0C='#88C0D0'
+hlwm_base0D='#81A1C1'
+hlwm_base0E='#B48EAD'
+hlwm_base0F='#5E81AC'
+
+herbstclient chain , \
+	, attr theme.color                        "${hlwm_base07}" \
+	, attr theme.background_color             "${hlwm_base00}" \
+	, attr theme.normal.color                 "${hlwm_base01}" \
+	, attr theme.active.color                 "${hlwm_base08}" \
+	, attr theme.active.background_color      "${hlwm_base00}" \
+	, attr theme.urgent.color                 "${hlwm_base0A}" \
+	, attr theme.floating.active.color        "${hlwm_base08}" \
+	, attr theme.floating.urgent.color        "${hlwm_base0A}" \
+	, attr theme.floating.background_color    "${hlwm_base00}" \
+	, attr settings.frame_bg_normal_color     "${hlwm_base00}" \
+	, attr settings.frame_bg_active_color     "${hlwm_base08}" \
+	, attr settings.frame_border_normal_color "${hlwm_base00}" \
+	, attr settings.frame_border_active_color "${hlwm_base08}" \
+	, attr theme.title_color                  "${hlwm_base07}" \
+	, attr theme.active.title_color		  "${hlwm_base07}"
