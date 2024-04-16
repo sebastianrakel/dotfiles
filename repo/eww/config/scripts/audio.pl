dedffe98c564dfab3eabf6f32a5d5ade26ce9883
Sebastian Rakel <sebastian@devunit.eu>
2024-04-16 15:29:47 +0200

    Add audio popup to eww bar

---
+++ repo/eww/config/scripts/audio.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+sub playerctl {local $" = ' '; $_ = qx(playerctl @_); chomp; $_}
+
+sub player_label {
+    my ($player) = @_;
+    my %icons = (
+	firefox => '',
+	spotify => '',
+    );
+
+    my $icon = %icons{$player} // '';
+
+    print qq(\(label :class "audio-player-label" :text "${icon} ${player}"\))
+}
+
+sub run {
+    my @players = split('\n', playerctl('-l'));
+    print qq(\(box :class "popup-box" :orientation "v");
+    for my $instance (@players) {
+        my ($player) = split(/\./,$instance);
+	my $metadata = playerctl('-p', $player,'metadata', '-f', '"{{artist}};{{album}};{{title}}"');
+	my $image_url = playerctl('-p', $player,'metadata','mpris:artUrl');
+
+	print qq(\(box :orientation "h" :class "audio-player" :space-evenly false);
+	print qq(\(box :width 256 :height 256 :class "audio-thumb" :style 'background-image: url("$image_url")'\));
+	print qq(\(box :halign "center" :valign "center" :orientation "v" :class "audio-metadata" :space-evenly false :spacing 20);
+	player_label($player);
+	for my $item (split(';', $metadata)) {
+	    if ($item ne '') { print qq(\(label :text "$item" :width 300 :wrap true\)); }
+	}
+
+	print qq(\(box :halign "center" :space-evenly false :height 32);
+	print qq(\(box :halign "center" :orientation "h" :class "audio-media-buttons" :space-evenly false :spacing 10 :width 200);
+	print qq(\(button :class "audio-media-button" :onclick "playerctl -p ${player} previous" ''\));
+	if(playerctl('-p', $player, 'status') eq 'Playing') {
+	    print qq(\(button :class "audio-media-button" :width 20 :height 20 :onclick "playerctl -p ${player} pause" ''\));
+	} else {
+	    print qq(\(button :class "audio-media-button" :onclick "playerctl -p ${player} play" ''\));
+	}
+	print qq(\(button :class "audio-media-button" :onclick "playerctl -p ${player} stop" ''\));
+	print qq(\(button :class "audio-media-button" :onclick "playerctl -p ${player} next" ''\));
+
+	print qq(\)\)\)\));
+    }
+    print qq(\));
+}
+
+run;
