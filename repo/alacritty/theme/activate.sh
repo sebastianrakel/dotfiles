f4af3a73e2e2ab096f0f3dd74c8c7d3eec0a4015
Sebastian Rakel <sebastian@devunit.eu>
2024-05-28 14:42:45 +0200

    Fix path of alacritty theme

--- repo/alacritty/theme/activate.sh
+++ repo/alacritty/theme/activate.sh
@@ -1,9 +1,9 @@
 echo "Theme: Alacritty set"
-alacritty_theme_path="${HOME}/.config/alacritty/theme.yml"
+alacritty_theme_path="${HOME}/.config/alacritty/theme.toml"
 if [[ -h "${alacritty_theme_path}" ]]; then
     rm "${alacritty_theme_path}"
 fi
 
 ln -s "${HOME}/.base16-themes/alacritty/${theme_name}-256.toml" "${alacritty_theme_path}"
 
-touch "${HOME}/.config/alacritty/alacritty.yml"
+touch "${HOME}/.config/alacritty/alacritty.toml"
